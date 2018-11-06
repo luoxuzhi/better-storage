b4cecba04f1ef183482c816d700e1ad9a9669fa2
luoxuzhi <herolxz@163.com>
2018-11-06 21:41:20 +0800

    change package name

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output:{
     path:path.join(__dirname,'/dist/'),
     filename:'[name].js',
-    library:'better-storage',
+    library:'expires-storage',
     libraryTarget:'umd'
   },
   module:{
